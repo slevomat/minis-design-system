906c535f890fde2de8dfe466d00515060c658f5a
Lexislav <lexislav@centrum.cz>
2026-03-04 21:34:58 +0100

    import zbytku hlavní sady ikon

--- packages/icons/src/registry.ts
+++ packages/icons/src/registry.ts
@@ -1,43 +1,441 @@
+import { airballoon } from './icons/airballoon.js';
+import { animationProgram } from './icons/animation-program.js';
+import { applePay } from './icons/apple-pay.js';
+import { arrival } from './icons/arrival.js';
+import { arrowClassic } from './icons/arrow-classic.js';
 import { arrowDown } from './icons/arrow-down.js';
 import { arrowLeft } from './icons/arrow-left.js';
 import { arrowRight } from './icons/arrow-right.js';
 import { arrowUp } from './icons/arrow-up.js';
+import { avatar } from './icons/avatar.js';
+import { babysitting } from './icons/babysitting.js';
+import { beauty } from './icons/beauty.js';
+import { bed } from './icons/bed.js';
 import { bell } from './icons/bell.js';
+import { benefits } from './icons/benefits.js';
+import { bestseller } from './icons/bestseller.js';
+import { bikeCableway } from './icons/bike-cableway.js';
+import { bikeRental } from './icons/bike-rental.js';
+import { bikeRoom } from './icons/bike-room.js';
+import { bikeTrails } from './icons/bike-trails.js';
+import { bin } from './icons/bin.js';
+import { box } from './icons/box.js';
+import { bubble } from './icons/bubble.js';
+import { building } from './icons/building.js';
+import { bulb } from './icons/bulb.js';
+import { bus } from './icons/bus.js';
+import { bus1 } from './icons/bus1.js';
+import { cake } from './icons/cake.js';
+import { calendar } from './icons/calendar.js';
+import { calendarUpcoming } from './icons/calendar-upcoming.js';
+import { calendar1 } from './icons/calendar1.js';
+import { calendar2 } from './icons/calendar2.js';
+import { camera } from './icons/camera.js';
+import { car } from './icons/car.js';
+import { car1 } from './icons/car1.js';
+import { car2 } from './icons/car2.js';
+import { card } from './icons/card.js';
 import { cartFill } from './icons/cart-fill.js';
 import { cartOutline } from './icons/cart-outline.js';
+import { cashback } from './icons/cashback.js';
+import { categories } from './icons/categories.js';
 import { check } from './icons/check.js';
+import { checkbox } from './icons/checkbox.js';
+import { checkboxPartly } from './icons/checkbox-partly.js';
+import { chest } from './icons/chest.js';
+import { children } from './icons/children.js';
+import { childrenLift } from './icons/children-lift.js';
+import { childrenLiftPoma } from './icons/children-lift-poma.js';
+import { childrenPool } from './icons/children-pool.js';
+import { children1 } from './icons/children1.js';
+import { childrensCot } from './icons/childrens-cot.js';
+import { circleArrow } from './icons/circle-arrow.js';
+import { circleCheck } from './icons/circle-check.js';
 import { circleCheckFill } from './icons/circle-check-fill.js';
+import { circleClose } from './icons/circle-close.js';
 import { circleCloseFill } from './icons/circle-close-fill.js';
+import { circleDollarFill } from './icons/circle-dollar-fill.js';
 import { circleInfoFill } from './icons/circle-info-fill.js';
+import { circlePlus } from './icons/circle-plus.js';
+import { circlePlusFill } from './icons/circle-plus-fill.js';
+import { circleQuestion } from './icons/circle-question.js';
+import { clock } from './icons/clock.js';
 import { close } from './icons/close.js';
+import { closed } from './icons/closed.js';
+import { club } from './icons/club.js';
+import { contractSigning } from './icons/contract-signing.js';
+import { copy } from './icons/copy.js';
+import { countryside } from './icons/countryside.js';
+import { creditCard } from './icons/credit-card.js';
+import { crossCountryTrails } from './icons/cross-country-trails.js';
+import { crossroads } from './icons/crossroads.js';
+import { czech } from './icons/czech.js';
+import { defaultIcon } from './icons/default.js';
+import { departure } from './icons/departure.js';
+import { deposit } from './icons/deposit.js';
+import { dislike } from './icons/dislike.js';
+import { documentIcon } from './icons/document.js';
+import { document1 } from './icons/document1.js';
+import { document2 } from './icons/document2.js';
+import { download } from './icons/download.js';
+import { edit } from './icons/edit.js';
 import { error } from './icons/error.js';
+import { evCharger } from './icons/ev-charger.js';
+import { eventBusy } from './icons/event-busy.js';
+import { expand } from './icons/expand.js';
+import { eye } from './icons/eye.js';
+import { eyeOff } from './icons/eye-off.js';
+import { eye1 } from './icons/eye1.js';
+import { feedback } from './icons/feedback.js';
+import { fileText } from './icons/file-text.js';
+import { filters } from './icons/filters.js';
+import { fitness } from './icons/fitness.js';
+import { fksp } from './icons/fksp.js';
+import { flag } from './icons/flag.js';
+import { flake } from './icons/flake.js';
+import { food } from './icons/food.js';
+import { fun } from './icons/fun.js';
+import { gameRoom } from './icons/game-room.js';
+import { gift } from './icons/gift.js';
+import { globus } from './icons/globus.js';
+import { golf } from './icons/golf.js';
+import { goods } from './icons/goods.js';
+import { growth } from './icons/growth.js';
+import { handicap } from './icons/handicap.js';
 import { heart } from './icons/heart.js';
 import { heartFill } from './icons/heart-fill.js';
+import { hearts } from './icons/hearts.js';
+import { home } from './icons/home.js';
+import { home1 } from './icons/home1.js';
+import { iceberg } from './icons/iceberg.js';
+import { indoorPool } from './icons/indoor-pool.js';
+import { kidsCorner } from './icons/kids-corner.js';
+import { light } from './icons/light.js';
+import { like } from './icons/like.js';
+import { like1 } from './icons/like1.js';
+import { lock } from './icons/lock.js';
+import { mail } from './icons/mail.js';
+import { mail1 } from './icons/mail1.js';
+import { meal } from './icons/meal.js';
+import { meal1 } from './icons/meal1.js';
+import { menu } from './icons/menu.js';
+import { menuburger } from './icons/menuburger.js';
+import { message } from './icons/message.js';
+import { microfon } from './icons/microfon.js';
+import { no } from './icons/no.js';
+import { noCreditCard } from './icons/no-credit-card.js';
+import { noParking } from './icons/no-parking.js';
+import { noSignal } from './icons/no-signal.js';
+import { noSmoking } from './icons/no-smoking.js';
+import { noTransfer } from './icons/no-transfer.js';
+import { noWifi } from './icons/no-wifi.js';
+import { notPetFriendly } from './icons/not-pet-friendly.js';
+import { onMap } from './icons/on-map.js';
+import { oneContract } from './icons/one-contract.js';
+import { otherServices } from './icons/other-services.js';
+import { outdoorPool } from './icons/outdoor-pool.js';
+import { outside } from './icons/outside.js';
+import { ownKitchen } from './icons/own-kitchen.js';
+import { packageIcon } from './icons/package.js';
+import { parking } from './icons/parking.js';
+import { partnership } from './icons/partnership.js';
+import { paw } from './icons/paw.js';
+import { percentage } from './icons/percentage.js';
+import { personOutline } from './icons/person-outline.js';
+import { phone } from './icons/phone.js';
+import { photoGallery } from './icons/photo-gallery.js';
+import { plane } from './icons/plane.js';
+import { plane1 } from './icons/plane1.js';
+import { plane2 } from './icons/plane2.js';
+import { playground } from './icons/playground.js';
+import { position } from './icons/position.js';
+import { position1 } from './icons/position1.js';
+import { printer } from './icons/printer.js';
+import { qrCode } from './icons/qr-code.js';
+import { radioChecked } from './icons/radio-checked.js';
+import { radioUnchecked } from './icons/radio-unchecked.js';
+import { refresh } from './icons/refresh.js';
+import { reload } from './icons/reload.js';
+import { reservation } from './icons/reservation.js';
+import { restaurant } from './icons/restaurant.js';
+import { sad } from './icons/sad.js';
+import { saltCave } from './icons/salt-cave.js';
+import { sauna } from './icons/sauna.js';
+import { scooterRental } from './icons/scooter-rental.js';
+import { sea } from './icons/sea.js';
 import { search } from './icons/search.js';
 import { settings } from './icons/settings.js';
+import { share } from './icons/share.js';
+import { shareIos } from './icons/share-ios.js';
+import { signal } from './icons/signal.js';
+import { singleImage } from './icons/single-image.js';
+import { skiPass } from './icons/ski-pass.js';
+import { skiRoom } from './icons/ski-room.js';
+import { skiSchool } from './icons/ski-school.js';
+import { skiSlopes } from './icons/ski-slopes.js';
+import { skibus } from './icons/skibus.js';
+import { slevomat } from './icons/slevomat.js';
+import { slovakia } from './icons/slovakia.js';
+import { smartphone } from './icons/smartphone.js';
+import { smile } from './icons/smile.js';
+import { snowpark } from './icons/snowpark.js';
+import { sort } from './icons/sort.js';
+import { spaProcedures } from './icons/spa-procedures.js';
 import { star } from './icons/star.js';
 import { starFill } from './icons/star-fill.js';
+import { stay } from './icons/stay.js';
+import { stick } from './icons/stick.js';
+import { stop } from './icons/stop.js';
+import { suitcase } from './icons/suitcase.js';
+import { suitcaseChecked } from './icons/suitcase-checked.js';
+import { surroundings } from './icons/surroundings.js';
+import { swimmingAreas } from './icons/swimming-areas.js';
+import { switchIcon } from './icons/switch.js';
+import { team } from './icons/team.js';
+import { term } from './icons/term.js';
+import { thermalPool } from './icons/thermal-pool.js';
+import { train } from './icons/train.js';
+import { train1 } from './icons/train1.js';
+import { train2 } from './icons/train2.js';
+import { travel } from './icons/travel.js';
+import { truck } from './icons/truck.js';
+import { tumblr } from './icons/tumblr.js';
 import { user } from './icons/user.js';
+import { userpc } from './icons/userpc.js';
+import { users } from './icons/users.js';
+import { verified } from './icons/verified.js';
+import { voucherOutline } from './icons/voucher-outline.js';
+import { walk } from './icons/walk.js';
+import { wallet } from './icons/wallet.js';
+import { waterPark } from './icons/water-park.js';
+import { wellness } from './icons/wellness.js';
+import { wellnessProcedures } from './icons/wellness-procedures.js';
+import { wheelchair } from './icons/wheelchair.js';
+import { wheelchairNotAccessible } from './icons/wheelchair-not-accessible.js';
+import { whirlpoolBath } from './icons/whirlpool-bath.js';
+import { wifi } from './icons/wifi.js';
+import { wifi1 } from './icons/wifi1.js';
+import { wineCellar } from './icons/wine-cellar.js';
+import { woodenSoakingTub } from './icons/wooden-soaking-tub.js';
 
 export const iconRegistry = {
+  'airballoon': airballoon,
+  'animation-program': animationProgram,
+  'apple-pay': applePay,
+  'arrival': arrival,
+  'arrow-classic': arrowClassic,
   'arrow-down': arrowDown,
   'arrow-left': arrowLeft,
   'arrow-right': arrowRight,
   'arrow-up': arrowUp,
+  'avatar': avatar,
+  'babysitting': babysitting,
+  'beauty': beauty,
+  'bed': bed,
   'bell': bell,
+  'benefits': benefits,
+  'bestseller': bestseller,
+  'bike-cableway': bikeCableway,
+  'bike-rental': bikeRental,
+  'bike-room': bikeRoom,
+  'bike-trails': bikeTrails,
+  'bin': bin,
+  'box': box,
+  'bubble': bubble,
+  'building': building,
+  'bulb': bulb,
+  'bus': bus,
+  'bus1': bus1,
+  'cake': cake,
+  'calendar': calendar,
+  'calendar-upcoming': calendarUpcoming,
+  'calendar1': calendar1,
+  'calendar2': calendar2,
+  'camera': camera,
+  'car': car,
+  'car1': car1,
+  'car2': car2,
+  'card': card,
   'cart-fill': cartFill,
   'cart-outline': cartOutline,
+  'cashback': cashback,
+  'categories': categories,
   'check': check,
+  'checkbox': checkbox,
+  'checkbox-partly': checkboxPartly,
+  'chest': chest,
+  'children': children,
+  'children-lift': childrenLift,
+  'children-lift-poma': childrenLiftPoma,
+  'children-pool': childrenPool,
+  'children1': children1,
+  'childrens-cot': childrensCot,
+  'circle-arrow': circleArrow,
+  'circle-check': circleCheck,
   'circle-check-fill': circleCheckFill,
+  'circle-close': circleClose,
   'circle-close-fill': circleCloseFill,
+  'circle-dollar-fill': circleDollarFill,
   'circle-info-fill': circleInfoFill,
+  'circle-plus': circlePlus,
+  'circle-plus-fill': circlePlusFill,
+  'circle-question': circleQuestion,
+  'clock': clock,
   'close': close,
+  'closed': closed,
+  'club': club,
+  'contract-signing': contractSigning,
+  'copy': copy,
+  'countryside': countryside,
+  'credit-card': creditCard,
+  'cross-country-trails': crossCountryTrails,
+  'crossroads': crossroads,
+  'czech': czech,
+  'default': defaultIcon,
+  'departure': departure,
+  'deposit': deposit,
+  'dislike': dislike,
+  'document': documentIcon,
+  'document1': document1,
+  'document2': document2,
+  'download': download,
+  'edit': edit,
   'error': error,
+  'ev-charger': evCharger,
+  'event-busy': eventBusy,
+  'expand': expand,
+  'eye': eye,
+  'eye-off': eyeOff,
+  'eye1': eye1,
+  'feedback': feedback,
+  'file-text': fileText,
+  'filters': filters,
+  'fitness': fitness,
+  'fksp': fksp,
+  'flag': flag,
+  'flake': flake,
+  'food': food,
+  'fun': fun,
+  'game-room': gameRoom,
+  'gift': gift,
+  'globus': globus,
+  'golf': golf,
+  'goods': goods,
+  'growth': growth,
+  'handicap': handicap,
   'heart': heart,
   'heart-fill': heartFill,
+  'hearts': hearts,
+  'home': home,
+  'home1': home1,
+  'iceberg': iceberg,
+  'indoor-pool': indoorPool,
+  'kids-corner': kidsCorner,
+  'light': light,
+  'like': like,
+  'like1': like1,
+  'lock': lock,
+  'mail': mail,
+  'mail1': mail1,
+  'meal': meal,
+  'meal1': meal1,
+  'menu': menu,
+  'menuburger': menuburger,
+  'message': message,
+  'microfon': microfon,
+  'no': no,
+  'no-credit-card': noCreditCard,
+  'no-parking': noParking,
+  'no-signal': noSignal,
+  'no-smoking': noSmoking,
+  'no-transfer': noTransfer,
+  'no-wifi': noWifi,
+  'not-pet-friendly': notPetFriendly,
+  'on-map': onMap,
+  'one-contract': oneContract,
+  'other-services': otherServices,
+  'outdoor-pool': outdoorPool,
+  'outside': outside,
+  'own-kitchen': ownKitchen,
+  'package': packageIcon,
+  'parking': parking,
+  'partnership': partnership,
+  'paw': paw,
+  'percentage': percentage,
+  'person-outline': personOutline,
+  'phone': phone,
+  'photo-gallery': photoGallery,
+  'plane': plane,
+  'plane1': plane1,
+  'plane2': plane2,
+  'playground': playground,
+  'position': position,
+  'position1': position1,
+  'printer': printer,
+  'qr-code': qrCode,
+  'radio-checked': radioChecked,
+  'radio-unchecked': radioUnchecked,
+  'refresh': refresh,
+  'reload': reload,
+  'reservation': reservation,
+  'restaurant': restaurant,
+  'sad': sad,
+  'salt-cave': saltCave,
+  'sauna': sauna,
+  'scooter-rental': scooterRental,
+  'sea': sea,
   'search': search,
   'settings': settings,
+  'share': share,
+  'share-ios': shareIos,
+  'signal': signal,
+  'single-image': singleImage,
+  'ski-pass': skiPass,
+  'ski-room': skiRoom,
+  'ski-school': skiSchool,
+  'ski-slopes': skiSlopes,
+  'skibus': skibus,
+  'slevomat': slevomat,
+  'slovakia': slovakia,
+  'smartphone': smartphone,
+  'smile': smile,
+  'snowpark': snowpark,
+  'sort': sort,
+  'spa-procedures': spaProcedures,
   'star': star,
   'star-fill': starFill,
+  'stay': stay,
+  'stick': stick,
+  'stop': stop,
+  'suitcase': suitcase,
+  'suitcase-checked': suitcaseChecked,
+  'surroundings': surroundings,
+  'swimming-areas': swimmingAreas,
+  'switch': switchIcon,
+  'team': team,
+  'term': term,
+  'thermal-pool': thermalPool,
+  'train': train,
+  'train1': train1,
+  'train2': train2,
+  'travel': travel,
+  'truck': truck,
+  'tumblr': tumblr,
   'user': user,
+  'userpc': userpc,
+  'users': users,
+  'verified': verified,
+  'voucher-outline': voucherOutline,
+  'walk': walk,
+  'wallet': wallet,
+  'water-park': waterPark,
+  'wellness': wellness,
+  'wellness-procedures': wellnessProcedures,
+  'wheelchair': wheelchair,
+  'wheelchair-not-accessible': wheelchairNotAccessible,
+  'whirlpool-bath': whirlpoolBath,
+  'wifi': wifi,
+  'wifi1': wifi1,
+  'wine-cellar': wineCellar,
+  'wooden-soaking-tub': woodenSoakingTub,
 } as const;
